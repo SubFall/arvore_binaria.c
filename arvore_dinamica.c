44ff609d0165f72aae54829400d122f6c83fe51b
Thalysom <thalysom.wolfgram@gmail.com>
2023-06-17 20:26:21 -0400

    Árvore Binária

---
+++ arvore_dinamica.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef struct NO{
+    int dado;
+    struct NO* pai;
+    struct NO* esq;
+    struct NO* dir;
+}NO;
+
+typedef struct ARVORE{
+    NO* raiz; 
+}ARVORE;
+
+ARVORE a;
+
+void inserirNo(int dado);
+void preOrdem(NO* raiz);
+void emOrdem(NO* raiz);
+void posOrdem(NO* raiz);
+void liberarMemoria(NO* no);
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    a.raiz = NULL;
+
+    inserirNo(10);
+    inserirNo(2);
+    inserirNo(4);
+    inserirNo(1);
+    inserirNo(13);
+    inserirNo(11);
+    preOrdem(a.raiz);
+    printf("\n\n");
+    emOrdem(a.raiz);
+    printf("\n\n");
+    posOrdem(a.raiz);
+    printf("\n");
+    liberarMemoria(a.raiz);//chama a funcão para liberar a memória
+}
+
+void inserirNo(int dado){
+    NO* ptr = (NO*)malloc(sizeof(NO));//aloca váriavel dinamicamente
+    if(ptr == NULL){//verifica se o ponteiro é null
+        printf("Erro ao alocar memória\n");
+        return;
+    }
+    //inicializar o ponteiro
+    ptr->dado = dado;
+    ptr->pai = NULL;
+    ptr->dir = NULL;
+    ptr->esq = NULL;
+
+    //se for o primeiro NÓ, vai ser a raiz
+    if(a.raiz == NULL){
+        a.raiz = ptr;
+        return;
+    }else{
+        NO* current = a.raiz;
+        NO* pai;
+        while(current != NULL){
+            pai = current;
+            if(ptr->dado < pai->dado){
+                current = current->esq;
+                if(current == NULL){
+                    pai->esq = ptr;
+                    printf("NO filho: %d recebe o NO pai a esquerda: \n", ptr->dado);
+                    return;
+                }
+            }else {
+                current = current->dir;
+                if(ptr->dado > pai->dado){
+                    if(current == NULL){
+                        pai->dir = ptr;
+                        printf("NO filho: %d recebe o NO pai a direita: \n", ptr->dado);
+                        return;
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
+
+//funcão para desalocar memória
+void liberarMemoria(NO* no){
+    if(no == NULL){
+        return;
+    }
+
+    liberarMemoria(no->esq);
+    liberarMemoria(no->dir);
+
+    free(no);
+}
+
+void preOrdem(NO* raiz){//caminhamento pre-ordem
+	if(raiz != NULL){
+		printf("%d \t", raiz->dado);
+		preOrdem(raiz->esq);
+		preOrdem(raiz->dir);
+	}
+}
+
+void emOrdem(NO* raiz){//caminhamento em-ordem
+    if(raiz != NULL){
+        emOrdem(raiz->esq);
+        printf("%d \t", raiz->dado);
+        emOrdem(raiz->dir);
+    }
+}
+
+void posOrdem(NO* raiz){//caminhamento pos-ordem
+    if(raiz != NULL){
+        posOrdem(raiz->esq);
+        posOrdem(raiz->dir);
+        printf("%d \t", raiz->dado);
+    }
+}
